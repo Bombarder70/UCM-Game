00cacf0cc3367dc102d82e428b4d7ffe79646ae1
Bombarder70 <holespato@gmail.com>
2021-10-23 19:06:21 +0200

    ..

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -14,11 +14,11 @@ public class Movement : MonoBehaviour
     // Update is called once per frame  
     void Update()  
     {  
-        if (Input.GetKey(KeyCode.A))  
+        if (Input.GetKey(KeyCode.D))  
         {  
             transform.Translate(0.1f, 0f, 0f);  
         }  
-        if (Input.GetKey(KeyCode.D))  
+        if (Input.GetKey(KeyCode.A))  
         {  
             transform.Translate(-0.1f, 0f, 0f);  
         }  
