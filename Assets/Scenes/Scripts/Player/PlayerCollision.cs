fbd538027b0001e108ac9685b7d21c5f22b835e0
Bombarder70 <holespato@gmail.com>
2021-11-09 19:00:08 +0100

    Collisions

---
+++ Assets/Scenes/Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+  void OnCollisionEnter(Collision collision) {
+    if (collision.gameObject.name == "tree1") {
+      Debug.Log(collision.gameObject.name);
+    }
+  }
+
+  // Znicenie objektu
+  // Destroy(gameObject);
+  //
+
+}
