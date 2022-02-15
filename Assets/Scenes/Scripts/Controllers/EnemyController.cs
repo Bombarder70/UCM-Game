4bbe80fbc2296503bcc80c5e7b3d4461c49c6c2d
Bombarder70 <holespato@gmail.com>
2022-02-15 09:35:46 +0100

    EnemyController

---
+++ Assets/Scenes/Scripts/Controllers/EnemyController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float enemyLook = 10f;
+    
+    Transform target;
+    NavMeshAgent agent;
+
+    void Start() {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float distance = Vector3.Distance(target.position, transform.position);
+    
+        if (distance <= enemyLook) {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, enemyLook);
+    }
+}
