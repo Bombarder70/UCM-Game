2810eeb4312f7e8ad9b73c9a39a329d2d5645144
Bombarder70 <holespato@gmail.com>
2022-02-25 12:39:51 +0100

    updateScore

--- Assets/Scenes/Scripts/UI/AnswerCheck.cs
+++ Assets/Scenes/Scripts/UI/AnswerCheck.cs
@@ -9,12 +9,29 @@ public class AnswerCheck : MonoBehaviour
     public InputField input;
     public GameObject quest;
 
+    public IEnumerator UpdateScore() {
+        WWWForm form = new WWWForm();
+
+        //form.AddField("nickname", "Pirat2");
+        form.AddField("score", Score.score);
+
+        WWW www = new WWW(
+            "http://localhost/holes/UcmGameWeb/index.php?action=update_score.php",
+            form
+        );
+
+        yield return www;
+        print(www.text);
+    }
+
     public void OnClick ()
     {
         if (input.text == CorrectAnswer)
         {
             if (HealthMonitor.HealthValue < 3) HealthMonitor.HealthValue += 1;
             Score.score += 100;
+
+            this.UpdateScore();
         }
         else
         {
