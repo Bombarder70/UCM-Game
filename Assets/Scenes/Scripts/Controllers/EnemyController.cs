cb05f92933f1daeb8b4b969dd85f213539accbc9
Bombarder70 <holespato@gmail.com>
2022-02-25 12:08:34 +0100

    Animations

--- Assets/Scenes/Scripts/Controllers/EnemyController.cs
+++ Assets/Scenes/Scripts/Controllers/EnemyController.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 public class EnemyController : MonoBehaviour
 {
     public float enemyLook = 10f;
+    public Animator enemyAnimator;
     
     Transform target;
     NavMeshAgent agent;
@@ -13,14 +14,23 @@ public class EnemyController : MonoBehaviour
     void Start() {
         target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
+
+        enemyAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+
     void Update() {
         float distance = Vector3.Distance(target.position, transform.position);
-    
-        if (distance <= enemyLook) {
+
+        if (distance < 1.5) {
+            enemyAnimator.SetBool("isRunning", false);
+            enemyAnimator.SetBool("isAttacking", true);
+        } else if (distance <= enemyLook) {
+            enemyAnimator.SetBool("isRunning", true);
+            enemyAnimator.SetBool("isAttacking", false);
             agent.SetDestination(target.position);
+        } else {
+            enemyAnimator.SetBool("isRunning", false);
         }
     }
 
