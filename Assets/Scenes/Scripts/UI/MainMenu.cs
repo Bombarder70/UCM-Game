153809d61b39b09391e00e532c6c6b0322932611
Juraj7777 <juraj.uhrina@gmail.com>
2022-03-14 16:44:10 +0100

    Main Menu

--- Assets/Scenes/Scripts/UI/MainMenu.cs
+++ Assets/Scenes/Scripts/UI/MainMenu.cs
@@ -2,12 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject loadingScreen;
+    public Slider slider;
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
+        StartCoroutine(LoadAsynchronously());
+    }
+
+    IEnumerator LoadAsynchronously()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(1);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            
+            slider.value = progress;
+
+            yield return null;
+        }   
     }
 
     public void LoadGame()
@@ -34,6 +53,7 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
 
 
 
