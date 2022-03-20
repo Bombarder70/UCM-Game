e5870e883f3ec7d78ece57b502cc4ffcdd609c80
Bombarder70 <holespato@gmail.com>
2022-03-20 19:41:12 +0100

    Correct answer

--- Assets/Scenes/Scripts/UI/AnswerCheck.cs
+++ Assets/Scenes/Scripts/UI/AnswerCheck.cs
@@ -4,37 +4,44 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 
-public class AnswerCheck : MonoBehaviour
-{
-    public string CorrectAnswer = "700";
+public class AnswerCheck : MonoBehaviour {
     public InputField input;
     public GameObject quest;
 
+		public class Odpoved {
+			public int id;
+			public string odpoved;
+			public int typ;
+		}
+
     public IEnumerator UpdateScore() {
 
-        WWWForm form = new WWWForm();
+			WWWForm form = new WWWForm();
 
-        form.AddField("score", Score.score);
+			form.AddField("score", Score.score);
 
-        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/holes/UcmGameWeb/web/index.php?action=update_score", form)) {
-            yield return www.SendWebRequest();
-        }
+			using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/holes/UcmGameWeb/web/index.php?action=update_score", form)) {
+				yield return www.SendWebRequest();
+			}
     }
 
-    public void OnClick ()
-    {
-        if (input.text == CorrectAnswer)
-        {
-            if (HealthMonitor.HealthValue < 3) HealthMonitor.HealthValue += 1;
-            Score.score += 100;
-    
-            StartCoroutine(this.UpdateScore());
-        }
-        else
-        {
-           HealthMonitor.HealthValue -= 1; 
-        }
+    public void OnClick () {
 
-        quest.SetActive(false);
+			// Prejdi odpovede ak sa tam nachadza tak true
+			foreach (string odpoved in ShowQuest.questAnswers) {
+				if (input.text == odpoved) {
+					if (HealthMonitor.HealthValue < 3) HealthMonitor.HealthValue += 1;
+					Score.score += 100;
+
+					StartCoroutine(this.UpdateScore());
+					Debug.Log("Spravna odpoved");
+					break;
+				} else {
+					HealthMonitor.HealthValue -= 1; 
+					Debug.Log("Nespravna odpoved");
+				}
+			}
+
+			quest.SetActive(false);
     }
 }
--- Assets/Scenes/Scripts/UI/ShowQuest.cs
+++ Assets/Scenes/Scripts/UI/ShowQuest.cs
@@ -3,50 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ShowQuest : MonoBehaviour
-{
-    public GameObject quest;
+public class ShowQuest : MonoBehaviour {
+	public GameObject quest;
 
-		public Text otazkaText;
-    public TextAsset jsonFile;
+	public Text otazkaText;
+	public TextAsset jsonFile;
 
-		[System.Serializable]
-    public class Quests {
-      public Quest[] quests;
-    }
+	public static List<string> questAnswers = new List<string>(); 
 
-    [System.Serializable]
-    public class Odpoved {
-      public int id;
-      public string odpoved;
-      public int typ;
-    }
+	[System.Serializable]
+	public class Quests {
+		public Quest[] quests;
+	}
 
-    [System.Serializable]
-    public class Quest {
-      public int id;
-      public string otazka;
-      public int typ;
-      public Odpoved[] odpovede;
-      public bool zobrazena;
-      public bool odpoved;
-    }
+	[System.Serializable]
+	public class Odpoved {
+		public int id;
+		public string odpoved;
+		public int typ;
+	}
 
-		void loadQuestFromJson() {
-      Quests questsInJson = JsonUtility.FromJson<Quests>(jsonFile.text);
+	[System.Serializable]
+	public class Quest {
+		public int id;
+		public string otazka;
+		public int typ;
+		public Odpoved[] odpovede;
+		public bool zobrazena;
+		public bool odpoved;
+	}
 
-      foreach (Quest quest in questsInJson.quests) {
-        if (quest.zobrazena == false) {
-          otazkaText.text = quest.otazka;
-          break;
-        }
-      }
-    }
+	void loadQuestFromJson() {
+		Quests questsInJson = JsonUtility.FromJson<Quests>(jsonFile.text);
 
-    void OnTriggerEnter () {
-			quest.SetActive (true);
-			Debug.Log("ready");
+		foreach (Quest quest in questsInJson.quests) {
+			if (quest.zobrazena == false) {
+				otazkaText.text = quest.otazka;
 
-			this.loadQuestFromJson();
-    }
+				foreach (Odpoved odpoved in quest.odpovede) {
+					ShowQuest.questAnswers.Add(odpoved.odpoved);
+				}
+
+				//ShowQuest.questAnswers = quest.odpovede;
+
+				break;
+			}
+		}
+	}
+
+	void OnTriggerEnter () {
+		quest.SetActive (true);
+		Debug.Log("ready");
+
+		this.loadQuestFromJson();
+	}
 }
