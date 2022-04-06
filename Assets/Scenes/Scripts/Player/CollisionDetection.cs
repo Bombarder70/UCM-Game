1baa6337c234699e229d8c0885360d216a0a7d42
Bombarder70 <holespato@gmail.com>
2022-04-06 22:14:20 +0200

    collisiondetection

--- Assets/Scenes/Scripts/Player/CollisionDetection.cs
+++ Assets/Scenes/Scripts/Player/CollisionDetection.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCollision : MonoBehaviour {
+public class CollisionDetection : MonoBehaviour {
 
   void OnCollisionEnter(Collision collision) {
     if (collision.gameObject.name != "Ostrov") {
