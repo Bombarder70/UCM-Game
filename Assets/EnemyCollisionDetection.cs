fde4ce534602d55bd1885e999499f0a9f3ebb236
Bombarder70 <holespato@gmail.com>
2022-04-11 20:04:43 +0200

    getHit

--- Assets/EnemyCollisionDetection.cs
+++ Assets/EnemyCollisionDetection.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class EnemyCollisionDetection : MonoBehaviour {
 
 	public Animator enemyAnimator;
+	private Animator playerAnimator;
+
+	void Start() {
+		GameObject pirat = GameObject.FindWithTag("pirat");
+ 
+		if (pirat != null) {
+			Debug.Log(pirat);
+			this.playerAnimator = pirat.GetComponent<Animator>();
+		}
+	}
 
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "sword") {
-			enemyAnimator.SetBool("getHit", true);
+			if (this.playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Attack")) {
+				Debug.Log(1);
+				enemyAnimator.SetBool("getHit", true);
+			}
 		}
 	}
 
