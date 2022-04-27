b580d2c295a54d58a505fbafac7d88c3a411fb68
Bombarder70 <holespato@gmail.com>
2022-04-27 20:59:43 +0200

    Some fixes

--- Assets/Scenes/Scripts/Player/PlayerManager.cs
+++ Assets/Scenes/Scripts/Player/PlayerManager.cs
@@ -6,8 +6,9 @@ public class PlayerManager : MonoBehaviour {
     
     public static PlayerManager instance;
 
-    public static string nickname;
-    public static int idGenerator;
+    public static string nickname; // player nickname
+    public static int idGenerator; // current id generator
+    public static int idPlayerGenerator; // current player id generator
 
     void Awake () {
         instance = this;
